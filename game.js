4a94ee6fee54e1a3460264c081ddf8bef3c3f5ce
Nereo Ontiveros <nerontiveros@gmail.com>
2020-09-03 10:55:13 +0100

    Add play the sound

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
-var buttonColours = ["red","blue","green","yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 
 
-function nextSequence(){
-  var randomNumber = Math.floor(Math.random()*4);
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
 
   gamePattern.push(randomChosenColour);
 
-  $('#'+randomChosenColour).fadeTo(100, 0.3, function() { $(this).fadeTo(500, 1.0); });
+  $('#' + randomChosenColour).fadeTo(100, 0.3, function() {
+    $(this).fadeTo(500, 1.0);
+  });
+
+  var sound = new Audio('sounds/'+randomChosenColour+'.mp3');
+  sound.play();
 }
