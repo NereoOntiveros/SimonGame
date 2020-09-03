20ad43b5cc172130968435a68cf237cb9c28998a
Nereo Ontiveros <nerontiveros@gmail.com>
2020-09-03 13:51:21 +0100

    Add check users answer

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # SimonGame
 A game modeled off the popular game Simon, using jQuery.
-The way that it works is that the user can press any key to start and it will show a tile that the user has to click on to follow a pattern.
+The way that it works is that the user can press any key to start and it will show a tile that the user has to click on to follow a pattern:
+
+Firstly the game shows the first color in the sequence (lets say blue).
+The user clicks the blue color.
+
+Next, the game shows the next color (lets say red), the user has to remember the sequence is
+blue, red and so on.
+
+If the user messes up the sequence the game ends.
--- game.js
+++ game.js
@@ -1,18 +1,41 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
-var userClickedPattern=[];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
 
-$(".btn").on('click',function(){
+
+
+$(document).keypress(function() {
+
+  if (!started) {
+    $('#level-title').text('Level ' + level);
+    nextSequence();
+    started = true;
+  }
+
+});
+
+
+
+$(".btn").on('click', function() {
   var userChosenColour = $(this).attr('id');
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 
 });
 
 
 function nextSequence() {
+
+  userClickedPattern = [];
+
+  level++;
+  $('#level-title').text('Level ' + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
 
@@ -26,13 +49,31 @@ function nextSequence() {
 
 }
 
-function playSound(name){
-  var sound = new Audio('sounds/'+name+'.mp3');
+function playSound(name) {
+  var sound = new Audio('sounds/' + name + '.mp3');
   sound.play();
 }
 
-function animatePress(currentColour){
-  $("#"+currentColour).addClass("pressed");
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
 
-  setTimeout(function(){ $("#"+currentColour).removeClass("pressed"); }, 100);
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log('Success');
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log('wrong');
+  }
+
+
 }
