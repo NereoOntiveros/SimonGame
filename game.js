abb684210fab2790666407ebbcc04a63cac59083
Nereo Ontiveros <nerontiveros@gmail.com>
2020-09-03 10:51:02 +0100

    Add random sequence and animation

---
+++ game.js
@@ -0,0 +1,13 @@
+var buttonColours = ["red","blue","green","yellow"];
+var gamePattern = [];
+
+
+
+function nextSequence(){
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $('#'+randomChosenColour).fadeTo(100, 0.3, function() { $(this).fadeTo(500, 1.0); });
+}
