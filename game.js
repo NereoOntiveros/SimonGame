ebf4aa5a5a8253a4f615f267c15507c507965c1f
Nereo Ontiveros <nerontiveros@gmail.com>
2020-09-03 11:47:13 +0100

    Add animations to user clicks

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ $(".btn").on('click',function(){
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
+  animatePress(userChosenColour);
 
 });
 
@@ -28,4 +29,10 @@ function nextSequence() {
 function playSound(name){
   var sound = new Audio('sounds/'+name+'.mp3');
   sound.play();
+}
+
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+
+  setTimeout(function(){ $("#"+currentColour).removeClass("pressed"); }, 100);
 }
